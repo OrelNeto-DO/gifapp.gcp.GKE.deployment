9102102d7eff0f22d69b3779a45d43b3113f4fdf
OrelNeto-DO <orelnetodo@gmail.com>
2025-02-10 10:39:30 +0200

    first time added app.py exporter coded. next commit would be sidecar with deployment for NodeExporter

--- src/app.py
+++ src/app.py
@@ -3,23 +3,30 @@ from sqlalchemy import create_engine, text
 import os
 import random
 from dotenv import load_dotenv
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 # Load environment variables from the .env file
 load_dotenv()
 
 app = Flask(__name__)
 
+# Prometheus metrics
+VISITORS = Counter('flask_app_visitors_total', 'Number of visitors to the website')
+
 # Read environment variables
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "mysql")  # Default to "mysql" if not set
-DB_PORT = os.getenv("DB_PORT", "3306")   # Default to "3306" if not set
-DB_NAME = os.getenv("DB_NAME", "mydatabase")  # Default to "mydatabase" if not set
+DB_HOST = os.getenv("DB_HOST", "mysql")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "mydatabase")
 
 # MySQL connection string
 DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DB_URL)
 
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 @app.route("/")
 def index():
@@ -38,9 +45,10 @@ def index():
         result = connection.execute(text("SELECT count FROM visit_count"))
         visitor_count = result.fetchone()["count"]
 
+        # Increment Prometheus counter
+        VISITORS.inc()
+
     return render_template("index.html", url=url, visitor_count=visitor_count)
 
-
 if __name__ == "__main__":
-    # Run the Flask app on 0.0.0.0 to make it accessible externally
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
