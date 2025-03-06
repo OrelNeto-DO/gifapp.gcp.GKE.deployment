3d127332b66d6564d3a17b402da112d0c32e943e
OrelNeto-DO <orelnetodo@gmail.com>
2025-03-06 02:12:38 +0200

    if this one doesnt work we will head back to the commit that i saved for a rough day

--- terraform/network_security.tf
+++ terraform/network_security.tf
@@ -1,3 +1,16 @@
+# קבלת מידע על קלאסטר ה-AKS הקיים
+data "azurerm_kubernetes_cluster" "existing" {
+  name                = var.kubernetes_cluster_name
+  resource_group_name = var.resource_group_name
+}
+
+# קבלת שם ה-NSG
+data "azurerm_resources" "node_nsg" {
+  resource_group_name = data.azurerm_kubernetes_cluster.existing.node_resource_group
+  type                = "Microsoft.Network/networkSecurityGroups"
+}
+
+# הוספת כלל HTTP לאפשר גישה בפורט 80
 resource "azurerm_network_security_rule" "allow_ingress_http" {
   name                        = "AllowIngressHTTP"
   priority                    = 1001
@@ -8,10 +21,11 @@ resource "azurerm_network_security_rule" "allow_ingress_http" {
   destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.aks_rg.name
-  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+  resource_group_name         = data.azurerm_kubernetes_cluster.existing.node_resource_group
+  network_security_group_name = data.azurerm_resources.node_nsg.resources[0].name
 }
 
+# הוספת כלל HTTPS לאפשר גישה בפורט 443
 resource "azurerm_network_security_rule" "allow_ingress_https" {
   name                        = "AllowIngressHTTPS"
   priority                    = 1002
@@ -22,6 +36,6 @@ resource "azurerm_network_security_rule" "allow_ingress_https" {
   destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.aks_rg.name
-  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+  resource_group_name         = data.azurerm_kubernetes_cluster.existing.node_resource_group
+  network_security_group_name = data.azurerm_resources.node_nsg.resources[0].name
 }
